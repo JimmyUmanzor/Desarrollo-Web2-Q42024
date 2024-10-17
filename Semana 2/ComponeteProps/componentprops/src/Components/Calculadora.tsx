701d030c92c9238cd99ce84909d02e4a18ed3227
JimmyUmanzor <159740825+JimmyUmanzor@users.noreply.github.com>
2024-10-16 21:26:01 -0600

    Mi primer Tarea
    Hice:
    Suma
    Resta
    Multiplicación y
    Factorial

--- Semana 2/ComponeteProps/componentprops/src/Components/Calculadora.tsx
+++ Semana 2/ComponeteProps/componentprops/src/Components/Calculadora.tsx
@@ -13,9 +13,30 @@ function Suma(a: number,b:number){
     return a+b;
 }
 
+function Resta(a:number,b:number){
+  return a-b;
+}
+
+function Multiplicacion(a:number,b:number){
+  return a*b;
+}
+
+function Factorial (a:number){
+  let resultado = 1
+  for (let i=2; i<=a; i++) {
+    resultado *=i;
+  }
+  return resultado
+}
+
+
+
 function Calculadora(props:Parametros) {
   return (
-    <div>Suma de los valores es {Suma(props.a, props.b)}</div>
+    <><div>Suma de los valores es: {Suma(props.a, props.b)}</div>
+    <div>Resta de los valores es: {Resta(props.a, props.b)}</div>
+    <div>Multiplicación de los valores es: {Multiplicacion(props.a, props.b)}</div>
+    <div>Factorización de los valores es: {Factorial(props.a)}</div></>
   )
 }
 
